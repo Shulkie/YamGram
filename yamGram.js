db5b75a7ab98bfdbe7c5851f10c7e09ba1ffe695
Yamila Krstulovich <yamlapin@icloud.com>
2019-03-23 11:43:21 -0300

    html y principio del css

---
+++ yamGram.js
@@ -0,0 +1,6 @@
+//var texto = document.getElementByClassName('texto')
+//console.log(texto)
+
+//texto.classList.add('rojo') para que funciona tengo que 
+//tener esa clase en el css
+//modifica y agrega el color
